d601103fb297cad0b85128d0a6b0972ed1b5b082
jackie.jiang <jackie.jiang@transn.com>
2018-09-06 10:51:59 +0800

    新增了主线程保护机制

--- TRDrop/Classes/TRDrop.m
+++ TRDrop/Classes/TRDrop.m
@@ -29,7 +29,6 @@ static TRDrop * _instace = nil;
         _instace = [super init];
         [_instace addSubview:self.imageV];
         [_instace addSubview:self.messageL];
-
     });
     return _instace;
 }
@@ -67,96 +66,98 @@ static TRDrop * _instace = nil;
     return _messageL;
 }
 +(void)showMessage:(NSString *)message state:(TRDropState)state duration:(CGFloat)duration{
- 
-    NSBundle *FrameWorkBundle = [NSBundle bundleForClass:[self class]];
-    NSBundle *dorpBundle  = [NSBundle bundleWithPath:[FrameWorkBundle pathForResource:@"Drop" ofType:@"bundle"]];
-    NSAssert(dorpBundle != nil, @"Bundle named '%@' not found!", @"Drop");
-    if (!dorpBundle) return;
- 
-    
-    TRDrop *drop =   [TRDrop sharedInstace];
-    if (drop) {
-        // 禁止将 AutoresizingMask 转换为 Constraints
-        drop.imageV.translatesAutoresizingMaskIntoConstraints = NO;
-        drop.messageL.translatesAutoresizingMaskIntoConstraints = NO;
-        // 添加 width 约束
-        NSLayoutConstraint *widthConstraint = [NSLayoutConstraint constraintWithItem:drop.imageV attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:0.0 constant:25];
-        [drop.imageV addConstraint:widthConstraint];
-        // 添加 height 约束
-        NSLayoutConstraint *heightConstraint = [NSLayoutConstraint constraintWithItem:drop.imageV attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:0.0 constant:25];
-        [drop.imageV addConstraint:heightConstraint];
-        // 添加 left 约束
-        NSLayoutConstraint *leftConstraint = [NSLayoutConstraint constraintWithItem:drop.imageV attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:drop attribute:NSLayoutAttributeLeft multiplier:1.0 constant:10];
-        [drop addConstraint:leftConstraint];
-        // 添加 centerX 约束
-        NSLayoutConstraint *centerXConstraint = [NSLayoutConstraint constraintWithItem:drop.imageV attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:drop attribute:NSLayoutAttributeCenterY multiplier:1 constant:0.5*[UIApplication sharedApplication].statusBarFrame.size.height];
-        [drop addConstraint:centerXConstraint];
- 
-        // 添加 height 约束
-        NSLayoutConstraint *heightConstraint1 = [NSLayoutConstraint constraintWithItem:drop.messageL attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:0.0 constant:50];
-        [drop.messageL addConstraint:heightConstraint1];
-        // 添加 left 约束
-        NSLayoutConstraint *leftConstraint1 = [NSLayoutConstraint constraintWithItem:drop.messageL attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:drop attribute:NSLayoutAttributeLeading multiplier:1.0 constant:55];
-        [drop addConstraint:leftConstraint1];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSBundle *FrameWorkBundle = [NSBundle bundleForClass:[self class]];
+        NSBundle *dorpBundle  = [NSBundle bundleWithPath:[FrameWorkBundle pathForResource:@"Drop" ofType:@"bundle"]];
+        NSAssert(dorpBundle != nil, @"Bundle named '%@' not found!", @"Drop");
+        if (!dorpBundle) return;
         
-        // 添加 right 约束
-        NSLayoutConstraint *rightConstraint1 = [NSLayoutConstraint constraintWithItem:drop.messageL attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:drop attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:20];
-        [drop addConstraint:rightConstraint1];
         
-        // 添加 centerX 约束
-        NSLayoutConstraint *centerXConstraint1 = [NSLayoutConstraint constraintWithItem:drop.messageL attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:drop attribute:NSLayoutAttributeCenterY multiplier:1 constant:0.5*[UIApplication sharedApplication].statusBarFrame.size.height];
-        [drop addConstraint:centerXConstraint1];
-    }
-    CGFloat height = 64+[UIApplication sharedApplication].statusBarFrame.size.height-20;
-    drop.frame = CGRectMake(0,-height, [UIScreen mainScreen].bounds.size.width, height);
-    drop.messageL.text = message;
-    switch (state) {
+        TRDrop *drop =   [TRDrop sharedInstace];
+        if (drop) {
+            // 禁止将 AutoresizingMask 转换为 Constraints
+            drop.imageV.translatesAutoresizingMaskIntoConstraints = NO;
+            drop.messageL.translatesAutoresizingMaskIntoConstraints = NO;
+            // 添加 width 约束
+            NSLayoutConstraint *widthConstraint = [NSLayoutConstraint constraintWithItem:drop.imageV attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:0.0 constant:25];
+            [drop.imageV addConstraint:widthConstraint];
+            // 添加 height 约束
+            NSLayoutConstraint *heightConstraint = [NSLayoutConstraint constraintWithItem:drop.imageV attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:0.0 constant:25];
+            [drop.imageV addConstraint:heightConstraint];
+            // 添加 left 约束
+            NSLayoutConstraint *leftConstraint = [NSLayoutConstraint constraintWithItem:drop.imageV attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:drop attribute:NSLayoutAttributeLeft multiplier:1.0 constant:10];
+            [drop addConstraint:leftConstraint];
+            // 添加 centerX 约束
+            NSLayoutConstraint *centerXConstraint = [NSLayoutConstraint constraintWithItem:drop.imageV attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:drop attribute:NSLayoutAttributeCenterY multiplier:1 constant:0.5*[UIApplication sharedApplication].statusBarFrame.size.height];
+            [drop addConstraint:centerXConstraint];
             
-        case TRDropStateInfo:
-        {
-            NSString *bundleImageName = [NSString stringWithFormat: @"%@", @"info_tip.png"];
-            drop.imageV.image = [[UIImage alloc] initWithContentsOfFile:[dorpBundle  pathForResource:bundleImageName ofType:nil]];
-            drop.backgroundColor = [UIColor whiteColor];
-            break;
+            // 添加 height 约束
+            NSLayoutConstraint *heightConstraint1 = [NSLayoutConstraint constraintWithItem:drop.messageL attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:0.0 constant:50];
+            [drop.messageL addConstraint:heightConstraint1];
+            // 添加 left 约束
+            NSLayoutConstraint *leftConstraint1 = [NSLayoutConstraint constraintWithItem:drop.messageL attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:drop attribute:NSLayoutAttributeLeading multiplier:1.0 constant:55];
+            [drop addConstraint:leftConstraint1];
+            
+            // 添加 right 约束
+            NSLayoutConstraint *rightConstraint1 = [NSLayoutConstraint constraintWithItem:drop.messageL attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:drop attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:20];
+            [drop addConstraint:rightConstraint1];
+            
+            // 添加 centerX 约束
+            NSLayoutConstraint *centerXConstraint1 = [NSLayoutConstraint constraintWithItem:drop.messageL attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:drop attribute:NSLayoutAttributeCenterY multiplier:1 constant:0.5*[UIApplication sharedApplication].statusBarFrame.size.height];
+            [drop addConstraint:centerXConstraint1];
         }
-        case TRDropStateSuccess:
-        {
-            NSString *bundleImageName = [NSString stringWithFormat: @"%@", @"success_tip.png"];
-            drop.imageV.image = [[UIImage alloc] initWithContentsOfFile:[dorpBundle  pathForResource:bundleImageName ofType:nil]];
-            drop.backgroundColor = [UIColor whiteColor];
-            break;
+        CGFloat height = 64+[UIApplication sharedApplication].statusBarFrame.size.height-20;
+        drop.frame = CGRectMake(0,-height, [UIScreen mainScreen].bounds.size.width, height);
+        drop.messageL.text = message;
+        switch (state) {
+                
+            case TRDropStateInfo:
+            {
+                NSString *bundleImageName = [NSString stringWithFormat: @"%@", @"info_tip.png"];
+                drop.imageV.image = [[UIImage alloc] initWithContentsOfFile:[dorpBundle  pathForResource:bundleImageName ofType:nil]];
+                drop.backgroundColor = [UIColor whiteColor];
+                break;
+            }
+            case TRDropStateSuccess:
+            {
+                NSString *bundleImageName = [NSString stringWithFormat: @"%@", @"success_tip.png"];
+                drop.imageV.image = [[UIImage alloc] initWithContentsOfFile:[dorpBundle  pathForResource:bundleImageName ofType:nil]];
+                drop.backgroundColor = [UIColor whiteColor];
+                break;
+            }
+                
+            case TRDropStateWarning:
+            {
+                NSString *bundleImageName = [NSString stringWithFormat: @"%@", @"info_tip.png"];
+                drop.imageV.image = [[UIImage alloc] initWithContentsOfFile:[dorpBundle  pathForResource:bundleImageName ofType:nil]];
+                drop.backgroundColor = [UIColor whiteColor];
+                break;
+            }
+            case TRDropStateError:
+            {
+                NSString *bundleImageName = [NSString stringWithFormat: @"%@", @"error_tip.png"];
+                drop.imageV.image = [[UIImage alloc] initWithContentsOfFile:[dorpBundle  pathForResource:bundleImageName ofType:nil]];
+                drop.backgroundColor = [UIColor whiteColor];
+                break;
+            }
+                
+            default:
+                break;
         }
-          
-        case TRDropStateWarning:
-        {
-            NSString *bundleImageName = [NSString stringWithFormat: @"%@", @"info_tip.png"];
-            drop.imageV.image = [[UIImage alloc] initWithContentsOfFile:[dorpBundle  pathForResource:bundleImageName ofType:nil]];
-            drop.backgroundColor = [UIColor whiteColor];
-            break;
-        }
-        case TRDropStateError:
-        {
-            NSString *bundleImageName = [NSString stringWithFormat: @"%@", @"error_tip.png"];
-            drop.imageV.image = [[UIImage alloc] initWithContentsOfFile:[dorpBundle  pathForResource:bundleImageName ofType:nil]];
-            drop.backgroundColor = [UIColor whiteColor];
-            break;
-        }
-         
-        default:
-            break;
-    }
-    [[UIApplication sharedApplication].delegate.window addSubview:drop];
-    [[UIApplication  sharedApplication].delegate.window bringSubviewToFront:drop];
-    [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
-        CGRect rect = drop.frame;
-        rect.origin.y = 0;
-        drop.frame = rect;
-    } completion:^(BOOL finished) {
-        
-    }];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [drop removeFromSuperview];
+        [[UIApplication sharedApplication].delegate.window addSubview:drop];
+        [[UIApplication  sharedApplication].delegate.window bringSubviewToFront:drop];
+        [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+            CGRect rect = drop.frame;
+            rect.origin.y = 0;
+            drop.frame = rect;
+        } completion:^(BOOL finished) {
+            
+        }];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [drop removeFromSuperview];
+        });
     });
+
 }
 +(void)showError:(NSString *)errorMsg{
     [TRDrop showMessage:errorMsg state:TRDropStateError duration:0.25];
